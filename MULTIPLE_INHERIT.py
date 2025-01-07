bf4766506ffabd1b9649317e390d39a76a6cfabc
Minhajur Rohoman <75428759+rahat9016@users.noreply.github.com>
2025-01-07 06:29:18 +0000

    inherit

--- MULTIPLE_INHERIT.py
+++ MULTIPLE_INHERIT.py
@@ -8,6 +8,23 @@ class B:
 class C(A,B):
     print('HELLO C')
 
+class Parent:
+    def greet(self):
+        print("Hello this parent")
 
-d = C()
-# print(d)
+class Child(Parent):
+    def greet(self):
+        print("Hello this Child")
+
+
+ch = Child().greet()
+
+class Car:
+    def __init__(self, model):
+        self.__model = model
+    
+    def get_model(self):
+        return self.__model
+
+
+print(Car("Hello").__model)
